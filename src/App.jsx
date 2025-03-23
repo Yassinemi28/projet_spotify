fac7483028c774280945c768d5cb80973e6281d2
yassine <ahmed-yassine.mimoune@epitech.eu>
2025-03-23 23:17:08 +0100

    all work on my side we gonna merge our work now

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/home/Home';
 import Artist from './components/artists/Artist';
 import Artists from './components/artists/Artists';
 
@@ -8,7 +7,6 @@ const App = () => {
   return (
     <Router>
       <Routes>  
-        <Route path="/" element={<Home />} /> 
         <Route path="/artists" element={<Artists />} /> 
         <Route path="/artist/:id" element={<Artist />} /> {/* Afficher le profil d'un artiste */}
       </Routes>
